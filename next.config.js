6a1d229b5a03f7d7934d0f8c8925c8ca08e6260a
qianwang-prenetics <qianwang@prenetics.com>
2026-04-19 16:37:21 +0800

    fix: disable PWA service worker to unblock magic link navigation

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
+  disable: true,
 });
 
 /** @type {import('next').NextConfig} */
